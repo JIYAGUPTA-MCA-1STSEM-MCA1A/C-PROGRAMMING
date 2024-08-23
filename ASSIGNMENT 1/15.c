668be564eee56059ea14b9719050d00c1d17af1f
JIYA <jiyag0057@gmail.com>
2024-08-23 15:25:54 +0530

    Create 15.c

---
+++ ASSIGNMENT 1/15.c
@@ -0,0 +1,15 @@
+//CHECKING NUMBER IS DVISIBLE BY 5 AND 11 WITHIN 100 TO 500
+) #include<stdio.h>
+ void main()
+ {
+ int num;
+printf("****PROGRAM TO CHECK IF A NUMBER IS DIVISIBLE BY 5 AND 11 WITH 100
+TO 500\n");
+ printf("NAME:JIYA GUPTA CLASS:MCA1A ROLLNO:27");
+ printf("enter any number:\n");
+ scanf("%d",&num);
+ if((num%5==0) && (num%11==0))
+ printf("number is divisible by 5 and 11");
+ else
+ printf("number is not divisible by 5 and 11");
+}
