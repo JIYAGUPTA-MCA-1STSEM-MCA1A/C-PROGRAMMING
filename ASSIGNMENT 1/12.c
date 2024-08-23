6064ccbac7feef337d086bc57c795d3597c2e154
JIYA <jiyag0057@gmail.com>
2024-08-23 15:20:07 +0530

    Create 12.c

---
+++ ASSIGNMENT 1/12.c
@@ -0,0 +1,14 @@
+//MAXIMUM BETWEEN TWO NUMBERS
+#include<stdio.h>
+ void main()
+ {
+ int a,b;
+ printf("****PROGRAM TO FIND THE MAXIMMUM BETWEEN 2 NUMBERS****\n");
+ printf("enter the first number:\n");
+ printf("enter the second number:\n");
+ scanf("%d %d",&a,&b);
+ if(a>b)
+ printf("a is the maximum between two number,");
+ else
+ printf("b is the maximum between two numbers");
+}
