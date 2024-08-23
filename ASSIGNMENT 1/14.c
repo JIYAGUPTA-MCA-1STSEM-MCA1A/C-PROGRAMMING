1931be0a99cc932007f9b7c2657cee2f54e6edd2
JIYA <jiyag0057@gmail.com>
2024-08-23 15:23:35 +0530

    Create 14.c

---
+++ ASSIGNMENT 1/14.c
@@ -0,0 +1,16 @@
+//POSTIVE, NEGATIVE OR ZERO
+#include<stdio.h>
+ void main()
+ {
+ int a;
+ printf("****PROGRAM TO CHECK IF A GIVEN NO IS POSITIVE OR NEGATIVE OR
+ZERO****\n");
+ printf("NAME:JIYA GUPTA CLASS:MCA1A ROLLNO:27\n");
+ scanf("%d",&a);
+ if(a>0)
+ printf("it is a positive number");
+ else if(a<0)
+ printf("it is negative number");
+ else
+ printf("it is 0");
+}
