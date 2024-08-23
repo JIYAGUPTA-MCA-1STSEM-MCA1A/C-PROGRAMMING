ff244e3d368fa04f68a255b1ed949d96c02796c6
JIYA <jiyag0057@gmail.com>
2024-08-23 14:44:49 +0530

    Update 1.c

--- ASSIGNMENT 1/1.c
+++ ASSIGNMENT 1/1.c
@@ -1,1 +1,15 @@
 // SUM AND AVERAGE OF 3 NUMBER
+#include<stdio.h>
+ void main()
+ {
+ int a,b,c,sum=0,avg;
+ printf("****PROGRAM TO FIND THE SUM AND AVERAGE OF 3 NUM****);
+ printf("NAME:JIYA GUPTA , CLASS:MCA1A , ROLLNO:27\n");
+ printf("Enter the first number:\n");
+ printf("Enter the second number:\n");
+ printf("Enter the third number:\n");
+ scanf("%d %d %d ",&a,&b,&c);
+ sum=a+b+c;
+ avg=sum/3;
+ printf("\n Sum and Average of %d %d,",sum,avg);
+ }
