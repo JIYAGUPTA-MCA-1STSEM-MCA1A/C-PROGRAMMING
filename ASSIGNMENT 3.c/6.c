79d229588cc7cb5b4aba32f2a8956884f42ce3b6
JIYA <jiyag0057@gmail.com>
2024-10-17 16:20:08 +0530

    Create 6.c

---
+++ ASSIGNMENT 3.c/6.c
@@ -0,0 +1,19 @@
+// PROGRAM TO INPUT A BINARY NUMBERIN AN ARRAY AND CONVERT IT INTO A CORRESPONDING DECIMAL NUMBER
+#include <stdio.h>
+int main() {
+int decimalNumber, binaryNumber[32], i = 0;
+printf(“NAME: JIYA GUPTA CLASS:MCA1A ROLLNO:27”);
+printf("Enter a decimal number: ");
+scanf("%d", &decimalNumber);
+while (decimalNumber > 0) {
+binaryNumber[i] = decimalNumber % 2;
+decimalNumber /= 2;
+i++;
+}
+printf("Binary equivalent: ");
+for (int j = i - 1; j >= 0; j--) {
+printf("%d", binaryNumber[j]);
+}
+printf("\n");
+return 0;
+}
